012baa84d99137b0aa34d050dcefb011f8632b83
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-08 21:28:57 -0400

    Added utilities for Prismic.

---
+++ src/utils/linkResolver.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link, RichText, Date } from "prismic-reactjs"
+
+// In src/prismic-configuration.js
+export const linkResolver = doc => {
+  // URL for a post type
+  if (doc.type === "post") {
+    return `/post/${doc.uid}`
+  }
+  // URL for a project type
+  if (doc.type === "project") {
+    return `/project/${doc.uid}`
+  }
+  // URL for a page type
+  if (doc.type === "page") {
+    return `/${doc.uid}`
+  }
+  // Backup for all other types
+  return "/"
+}
