3848c668c22e4b984e857eb33d90f7df05799464
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-12 13:16:27 -0400

    Added support for MDBReact.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,8 +6,22 @@
 
 // You can delete this file if you're not using it
 
+// Global Styles
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap-css-only/css/bootstrap.min.css"
+import "./src/styles/scss/mdb.scss"
+
 // Prismic Link Resolver Function
 const { registerLinkResolver } = require("gatsby-source-prismic-graphql")
 const { linkResolver } = require("./src/utils/linkResolver")
 
 registerLinkResolver(linkResolver)
+
+// Adds support for Gatsby Image Background plugin
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`)
+    console.log(`# IntersectionObserver is polyfilled!`)
+  }
+}
