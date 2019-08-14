1291bf0d04354d174cdd04eb77f001719f9e28bf
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-14 19:26:34 -0400

    Updated data for hero component.

--- src/components/slices/Hero.js
+++ src/components/slices/Hero.js
@@ -1,20 +1,52 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import BackgroundImage from "gatsby-background-image"
+//MDB Hero Components
+import {
+  MDBJumbotron,
+  MDBRow,
+  MDBCol,
+  MDBAnimation,
+  MDBBtn,
+  MDBIcon,
+  MDBContainer,
+} from "mdbreact"
 
-// import { Img } from "gatsby-image"
+function Hero({ input }) {
+  return (
+    <BackgroundImage fluid={input.background.localFile.childImageSharp.fluid}>
+      <MDBJumbotron fluid id="hero">
+        <MDBRow className="pt-2 pt-md-5 my-2 my-md-5 text-center">
+          <MDBContainer>
+            <MDBCol size="12" className="pt-5 mt-5 mx-auto">
+              <MDBAnimation type="fadeIn" delay="0.2s" reveal>
+                <h1 className="white-text pb-3">
+                  {/* I'm <span className="font-weight-bold">Z</span>.{" "}
+                  <br className="d-block d-md-none" />
+                  <span className="font-weight-bold">Web Developer</span>{" "}
+                  <br className="d-block d-md-none" />&{" "}
+                  <span className="font-weight-bold">Designer.</span> */}
+                  {input.title.text}
+                </h1>
+              </MDBAnimation>
 
-const Hero = ({ slice }) => (
-  // <MDBView>
-  //   <img
-  //     src={slice.primary.hero_background_image.url}
-  //     className="img-fluid"
-  //     alt="alt text"
-  //   />
-  // </MDBView>
-  <BackgroundImage
-    fluid={slice.primary.hero_background_imageSharp.childImageSharp.fluid}
-  ></BackgroundImage>
-)
-
+              <MDBRow className="py-2 py-md-4">
+                <MDBCol>
+                  <MDBAnimation type="fadeIn" delay="0.5s" reveal>
+                    <Link to={input.button_link.url} alt="dat alt">
+                      <MDBBtn color="primary" tag="span">
+                        <MDBIcon icon="folder-open" className="mr-2" />
+                        {input.button_title}
+                      </MDBBtn>
+                    </Link>
+                  </MDBAnimation>
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+          </MDBContainer>
+        </MDBRow>
+      </MDBJumbotron>
+    </BackgroundImage>
+  )
+}
 export default Hero
