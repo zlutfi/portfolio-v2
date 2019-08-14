bdb18a0bd583e350025e75d7fb3343d6a24a9dd3
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-14 17:42:47 -0400

    Added index for slice components.

---
+++ src/components/slices/index.js
@@ -0,0 +1,7 @@
+import BodyText from "./BodyText"
+import CodeBlock from "./CodeBlock"
+import Image from "./Image"
+import Quote from "./Quote"
+import Hero from "./Hero"
+
+export { Image, CodeBlock, BodyText, Quote, Hero }
