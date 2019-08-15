ee3b3c30fc8022b1fde4cbc5f18a4b714bc6a520
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-15 12:29:06 -0400

    Added contact form with validation.

---
+++ src/components/ContactForm.js
@@ -0,0 +1,172 @@
+import React from "react"
+import { Formik, Field } from "formik"
+import validationSchema from "../utils/validationSchema"
+import {
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBIcon,
+  MDBAlert,
+  MDBAnimation,
+} from "mdbreact"
+import { navigate } from "gatsby-link"
+
+// URI Encoder Component
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+const ContactForm = () => {
+  return (
+    <Formik
+      initialValues={{ name: "", email: "", phone: "", message: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        fetch("/?no-cache=1", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({
+            "form-name": "contact",
+            ...values,
+          }),
+        })
+          .then(() => {
+            navigate("/success")
+            setSubmitting(false)
+          })
+          .catch(error => {
+            alert("Error: Please Try Again!")
+            setSubmitting(false)
+          })
+      }}
+      render={({
+        touched,
+        errors,
+        isSubmitting,
+        handleSubmit,
+        handleReset,
+      }) => (
+        <form
+          className="form"
+          name="contact"
+          onSubmit={handleSubmit}
+          onReset={handleReset}
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="subject" value="Portfolio contact form" />
+          <MDBRow>
+            <MDBCol md="12" className="mb-0">
+              <div className="form-group">
+                <label htmlFor="name" className="label invisible">
+                  Name
+                </label>
+
+                <Field
+                  className="form-control form-control-md"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  aria-label="Name"
+                />
+                {touched.name && errors.name && (
+                  <MDBAnimation type="fadeIn">
+                    <MDBAlert className="danger mt-2" color="danger">
+                      {errors.name}
+                    </MDBAlert>
+                  </MDBAnimation>
+                )}
+              </div>
+            </MDBCol>
+            <MDBCol md="12" className="mb-0">
+              <div className="form-group">
+                <label htmlFor="email" className="label invisible">
+                  Email Address
+                </label>
+
+                <Field
+                  className="form-control form-control-md"
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  aria-label="Email"
+                />
+                {touched.email && errors.email && (
+                  <MDBAnimation type="fadeIn">
+                    <MDBAlert className="danger mt-2" color="danger">
+                      {errors.email}
+                    </MDBAlert>
+                  </MDBAnimation>
+                )}
+              </div>
+            </MDBCol>
+            <MDBCol md="12" className="mb-0">
+              <div className="form-group">
+                <label htmlFor="phone" className="label invisible">
+                  Phone Number
+                </label>
+
+                <Field
+                  className="form-control form-control-md"
+                  type="tel"
+                  name="phone"
+                  placeholder="Phone Number"
+                  aria-label="Phone"
+                />
+                {touched.phone && errors.phone && (
+                  <MDBAnimation type="fadeIn">
+                    <MDBAlert className="danger mt-2" color="danger">
+                      {errors.phone}
+                    </MDBAlert>
+                  </MDBAnimation>
+                )}
+              </div>
+            </MDBCol>
+            <MDBCol md="12" className="mb-0">
+              <div className="form-group">
+                <label htmlFor="message" className="label invisible">
+                  Message
+                </label>
+
+                <Field
+                  className="form-control"
+                  name="message"
+                  component="textarea"
+                  rows="6"
+                  placeholder="Message"
+                  aria-label="Message"
+                />
+
+                {touched.message && errors.message && (
+                  <MDBAnimation type="fadeIn">
+                    <MDBAlert className="danger mt-2" color="danger">
+                      {errors.message}
+                    </MDBAlert>
+                  </MDBAnimation>
+                )}
+              </div>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol className="text-center pt-2">
+              <MDBBtn
+                color="primary"
+                type="submit"
+                name="submit"
+                disabled={isSubmitting}
+                aria-label="Submit"
+              >
+                <MDBIcon icon="paper-plane" className="mr-2" />
+                Send Message
+              </MDBBtn>
+            </MDBCol>
+          </MDBRow>
+        </form>
+      )}
+    />
+  )
+}
+
+export default ContactForm
---
+++ src/utils/validationSchema.js
@@ -0,0 +1,22 @@
+import * as Yup from "yup"
+
+// regex for real phone number
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "This name is too short.")
+    .max(50, "This name is too long.")
+    .required("A name is required."),
+  email: Yup.string()
+    .email("Please enter a valid email address.")
+    .required("An email address is required."),
+  phone: Yup.string()
+    .matches(phoneRegExp, "Please enter a valid phone number.")
+    .min(10, "This phone number is too short.")
+    .max(50, "This phone number is too long.")
+    .required("Please enter a phone number."),
+  message: Yup.string().required("Please enter a message."),
+})
+
+export default validationSchema
