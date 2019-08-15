26195e7aa8d7e780b4636357e405b94a6d13281d
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-15 19:53:11 -0400

    Updated queries.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,97 +3,13 @@ import PropTypes from "prop-types"
 // import styled from '@emotion/styled'
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import { MDBContainer } from "mdbreact"
-// import website from '../../config/website'
-
-// const Hero = styled.header`
-//   background-color: ${props => props.theme.colors.greyLight};
-//   display: flex;
-//   align-items: center;
-// `
-
-// const HeroInner = styled(Wrapper)`
-//   padding-top: 13rem;
-//   padding-bottom: 13rem;
-//   h1 {
-//     margin-bottom: 2rem;
-//   }
-//   @media (max-width: ${props => props.theme.breakpoints.l}) {
-//     padding-top: 10rem;
-//     padding-bottom: 10rem;
-//   }
-//   @media (max-width: ${props => props.theme.breakpoints.m}) {
-//     padding-top: 8rem;
-//     padding-bottom: 8rem;
-//   }
-//   @media (max-width: ${props => props.theme.breakpoints.s}) {
-//     padding-top: 6rem;
-//     padding-bottom: 6rem;
-//   }
-// `
-
-// const HeroText = styled.div`
-//   font-size: 1.7rem;
-//   line-height: 1.4;
-//   margin-bottom: 2rem;
-//   @media (max-width: ${props => props.theme.breakpoints.m}) {
-//     font-size: 1.4rem;
-//   }
-//   @media (max-width: ${props => props.theme.breakpoints.s}) {
-//     font-size: 1.25rem;
-//   }
-// `
-
-// const hero = styled.ul`
-//   list-style-type: none;
-//   display: flex;
-//   flex-wrap: wrap;
-//   margin-left: 0;
-//   font-family: 'Source Sans Pro', -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial',
-//     sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-//   li {
-//     display: inline;
-//     &:not([data-name='hero-entry-0']) {
-//       margin-left: 2.5rem;
-//       @media (max-width: ${props => props.theme.breakpoints.s}) {
-//         margin-left: 1.75rem;
-//       }
-//     }
-//     a {
-//       font-style: normal;
-//       color: ${props => props.theme.colors.greyDark};
-//       font-size: 1.333rem;
-//       font-weight: 600;
-//       &:hover,
-//       &:focus {
-//         color: ${props => props.theme.colors.primary};
-//         text-decoration: none;
-//       }
-//       @media (max-width: ${props => props.theme.breakpoints.s}) {
-//         font-size: 1.2rem;
-//       }
-//     }
-//   }
-// `
-
-// const ProjectListing = styled.ul`
-//   list-style-type: none;
-//   margin-left: 0;
-//   margin-top: 4rem;
-//   li {
-//     margin-bottom: 1.45rem;
-//     a {
-//       font-size: 2.369rem;
-//       font-style: normal;
-//       color: ${props => props.theme.colors.black};
-//       @media (max-width: ${props => props.theme.breakpoints.s}) {
-//         font-size: 1.777rem;
-//       }
-//     }
-//   }
-// `
-
-// const IndexWrapper = Wrapper.withComponent('main')
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
+import Hero from "../components/slices/Hero"
+import Img from "gatsby-image"
+import SEO from "../components/seo"
+import ProjectsSection from "../components/slices/homepage_sections/ProjectsSection"
+import PostsSection from "../components/slices/homepage_sections/PostsSection"
+import SliceZone from "../components/SliceZone"
 
 class Index extends Component {
   render() {
@@ -102,37 +18,26 @@ class Index extends Component {
     } = this.props
     return (
       <Layout>
-        {/* <Hero> */}
-        <h1>{homepage.data.title.text}</h1>
-        {/* <HeroText dangerouslySetInnerHTML={{ __html: homepage.data.content.html }} /> */}
-        <MDBContainer>
-          {hero.nodes.map((s, index) => (
-            <li data-name={`hero-entry-${index}`} key={s.primary.text.text}>
-              <a href={s.primary.text.text}>{s.primary.text.text}</a>
-            </li>
-          ))}
-        </MDBContainer>
-        {/* </Hero> */}
-        {/* <IndexWrapper id={website.skipNavId} style={{ paddingTop: '2rem', paddingBottom: '2rem' }}> */}
-        <MDBContainer>
+        <SEO title={homepage.data.title} />
+        {hero.nodes.map((h, index) => (
+          <div key={index}>
+            <Hero input={h.primary} />
+          </div>
+        ))}
+        {/* <SliceZone allSlices={homepage.body} /> */}
+
+        {/* <MDBContainer>
           <h2 style={{ marginTop: "4rem" }}>Recent posts</h2>
-          {/* <Listing posts={posts.nodes} /> */}
-          {/* {posts.nodes} */}
-          {posts.nodes.map(post => (
-            <li key={post.id}>
-              <Link to={post.url}>{post.data.title.text}</Link>
+
+          {posts.edges.map(post => (
+            <li key={post.node.id}>
+              <Link to={post.node.url}>{post.node.data.title}</Link>
             </li>
           ))}
-        </MDBContainer>
-        <MDBContainer>
-          <h2 style={{ marginTop: "8rem" }}>Recent projects</h2>
-          {projects.nodes.map(project => (
-            <li key={project.id}>
-              <Link to={project.url}>{project.data.title.text}</Link>
-            </li>
-          ))}
-        </MDBContainer>
-        {/* </IndexWrapper> */}
+        </MDBContainer> */}
+        <PostsSection input={posts.nodes} />
+
+        <ProjectsSection input={projects.nodes} />
       </Layout>
     )
   }
@@ -144,9 +49,7 @@ Index.propTypes = {
   data: PropTypes.shape({
     homepage: PropTypes.shape({
       data: PropTypes.shape({
-        title: PropTypes.shape({
-          text: PropTypes.string.isRequired,
-        }),
+        title: PropTypes.string.isRequired,
         // content: PropTypes.shape({
         //   html: PropTypes.string.isRequired,
         // }),
@@ -168,16 +71,39 @@ export const pageQuery = graphql`
   query IndexQuery {
     homepage: prismicHomepage {
       data {
-        title {
-          text
-        }
+        title
       }
     }
-    hero: allPrismicHomepageBodyText {
+    hero: allPrismicHomepageBodyHero {
       nodes {
+        slice_type
         primary {
-          text {
+          title {
             text
+            html
+          }
+          button_title
+          button_link {
+            type
+            url
+          }
+          background {
+            localFile {
+              childImageSharp {
+                fluid(
+                  maxWidth: 1920
+                  maxHeight: 1080
+                  quality: 90
+                  duotone: {
+                    highlight: "#007bff"
+                    shadow: "#202932"
+                    opacity: 90
+                  }
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
           }
         }
       }
@@ -188,13 +114,66 @@ export const pageQuery = graphql`
         url
         type
         data {
-          title {
-            text
-          }
+          title
+          description
           date(formatString: "DD.MM.YYYY")
           categories {
             category {
               slug
+            }
+          }
+          hero {
+            localFile {
+              childImageSharp {
+                fluid(cropFocus: CENTER, maxHeight: 200, maxWidth: 400) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          body {
+            ... on PrismicPostBodyImage {
+              id
+              slice_type
+              primary {
+                image {
+                  localFile {
+                    childImageSharp {
+                      fixed {
+                        ...GatsbyImageSharpFixed_withWebp
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            ... on PrismicPostBodyText {
+              id
+              slice_type
+              primary {
+                text {
+                  html
+                }
+              }
+            }
+            ... on PrismicPostBodyQuote {
+              id
+              slice_type
+              primary {
+                quote {
+                  text
+                }
+              }
+            }
+            ... on PrismicPostBodyCodeBlock {
+              id
+              slice_type
+              primary {
+                code_block {
+                  html
+                  text
+                }
+              }
             }
           }
         }
@@ -203,14 +182,68 @@ export const pageQuery = graphql`
     projects: allPrismicProject {
       nodes {
         id
-        url
         type
+        url
         data {
-          description {
-            text
+          subtitle
+          title
+          hero {
+            localFile {
+              childImageSharp {
+                fluid(cropFocus: CENTER, maxHeight: 200, maxWidth: 400) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
           }
-          title {
-            text
+          category {
+            slug
+          }
+          body {
+            ... on PrismicProjectBodyImageGallery {
+              id
+              slice_type
+              items {
+                gallery_image {
+                  localFile {
+                    childImageSharp {
+                      fixed(height: 300, width: 300) {
+                        ...GatsbyImageSharpFixed_withWebp
+                      }
+                    }
+                  }
+                  alt
+                }
+                image_caption {
+                  text
+                }
+              }
+            }
+            ... on PrismicProjectBodyProjectInfo {
+              id
+              slice_type
+              primary {
+                project_link {
+                  url
+                  type
+                  link_type
+                }
+                project_code_link {
+                  url
+                  type
+                  link_type
+                }
+              }
+            }
+            ... on PrismicProjectBodyText {
+              id
+              slice_type
+              primary {
+                text {
+                  html
+                }
+              }
+            }
           }
         }
       }
