ad46d4c5a467dec79f5c3211ba8d2bc22054a045
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-15 12:33:08 -0400

    Added new slice components to index.

--- src/components/slices/index.js
+++ src/components/slices/index.js
@@ -3,5 +3,19 @@ import CodeBlock from "./CodeBlock"
 import Image from "./Image"
 import Quote from "./Quote"
 import Hero from "./Hero"
+import ProjectInfo from "./ProjectInfo"
+import ImageGallery from "./ImageGallery"
+import RecentPosts from "./RecentPosts"
+import RecentProjects from "./RecentProjects"
 
-export { Image, CodeBlock, BodyText, Quote, Hero }
+export {
+  Image,
+  CodeBlock,
+  BodyText,
+  Quote,
+  Hero,
+  ProjectInfo,
+  ImageGallery,
+  RecentPosts,
+  RecentProjects,
+}
