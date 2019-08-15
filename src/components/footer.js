76b0e0b0226c7b06b79e4d78578e11a8dbf4c6c6
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-15 19:52:24 -0400

    Moved inline SVG to bottom.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,5 @@
 import React from "react"
 import { MDBContainer, MDBFooter, MDBRow, MDBCol } from "mdbreact"
-const Logo = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 163.13 72.6"
-    className="footer-logo"
-  >
-    <defs>
-      <clipPath transform="translate(0 .02)">
-        <polygon points="163.13 72.58 91.3 72.58 91.3 -0.02 119.62 -0.02 119.62 64.44 163.13 64.1 163.13 72.58" />
-      </clipPath>
-    </defs>
-    <path
-      d="M86.17,72.58H0v-8.5L43.08,7.25,1.84,7.12,1.72,0H79.46V5.72L37.66,64.61l48.51-.51Z"
-      transform="translate(0 .02)"
-    />
-    <path
-      d="M163.13,72.58H91.3V0h28.32V64.44l43.51-.34Z"
-      transform="translate(0 .02)"
-    />
-    <polygon points="163.13 72.6 91.3 72.6 91.3 0 119.62 0 119.62 64.46 163.13 64.12 163.13 72.6" />
-    <g>
-      <path
-        d="M86.17,72.58H0v-8.5L43.08,7.25,1.84,7.12,1.72,0H79.46V5.72L37.66,64.61l48.51-.51Z"
-        transform="translate(0 .02)"
-      />
-    </g>
-  </svg>
-)
 
 const Footer = () => {
   return (
@@ -63,3 +35,34 @@ const Footer = () => {
 }
 
 export default Footer
+
+// Inline Logo SVG
+
+const Logo = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 163.13 72.6"
+    className="footer-logo"
+  >
+    <defs>
+      <clipPath transform="translate(0 .02)">
+        <polygon points="163.13 72.58 91.3 72.58 91.3 -0.02 119.62 -0.02 119.62 64.44 163.13 64.1 163.13 72.58" />
+      </clipPath>
+    </defs>
+    <path
+      d="M86.17,72.58H0v-8.5L43.08,7.25,1.84,7.12,1.72,0H79.46V5.72L37.66,64.61l48.51-.51Z"
+      transform="translate(0 .02)"
+    />
+    <path
+      d="M163.13,72.58H91.3V0h28.32V64.44l43.51-.34Z"
+      transform="translate(0 .02)"
+    />
+    <polygon points="163.13 72.6 91.3 72.6 91.3 0 119.62 0 119.62 64.46 163.13 64.12 163.13 72.6" />
+    <g>
+      <path
+        d="M86.17,72.58H0v-8.5L43.08,7.25,1.84,7.12,1.72,0H79.46V5.72L37.66,64.61l48.51-.51Z"
+        transform="translate(0 .02)"
+      />
+    </g>
+  </svg>
+)
