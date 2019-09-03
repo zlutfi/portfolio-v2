159dc0dc3874668444b190eee8ab3b9483b75d18
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-03 11:36:00 -0400

    Created sitemap component.

--- src/components/SiteMap.js
+++ src/components/SiteMap.js
@@ -1,33 +1,34 @@
-import React, { Component } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
 
-export default class SiteMap extends Component {
-  render() {
-    const { input } = this.props
-    return (
-      <MDBContainer>
-        <MDBRow size="12" className="py-5">
-          <MDBCol>
-            <h2>Sitemap</h2>
-          </MDBCol>
-        </MDBRow>
-        <MDBRow className="pb-5">
-          <MDBCol size="6" md="4">
-            <ul>
-              {input.node.map(page => (
-                <li key={page.id}>
-                  <Link to={page.url}>{page.data.title}</Link>
-                </li>
-              ))}
-            </ul>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    )
-  }
-}
+const SiteMap = ({ input }) => (
+  <MDBContainer>
+    <MDBRow size="12" className="py-5">
+      <MDBCol>
+        <h2>Sitemap</h2>
+      </MDBCol>
+    </MDBRow>
+    <MDBRow className="pb-5">
+      <MDBCol size="6" md="4">
+        <ul>
+          {input.allPrismicPage.edges.map(page => (
+            <li key={page.node.id}>
+              <Link to={page.node.url}>{page.node.data.title}</Link>
+            </li>
+          ))}
+          {input.allPrismicStaticPage.edges.map(page => (
+            <li key={page.node.id}>
+              <Link to={page.node.url}>{page.node.data.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </MDBCol>
+    </MDBRow>
+  </MDBContainer>
+)
+export default SiteMap
 
 SiteMap.propTypes = {
   input: PropTypes.object.isRequired,
