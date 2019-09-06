76725426dab0fcf9d311b4eafd45c3bc7caffb2c
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-06 12:58:46 -0400

    Converted footer content to data.

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,74 +1,85 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { MDBContainer, MDBRow, MDBCol, MDBFooter } from "mdbreact"
+import { OutboundLink } from "gatsby-plugin-google-analytics"
+import { PropTypes } from "prop-types"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      prismicFooter {
+        data {
+          copyright
+          powered_by_title
+          powered_by {
+            link {
+              url
+              target
+            }
+            image {
+              url
+              alt
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
-    <>
-      <MDBFooter className="py-5">
-        <MDBContainer>
-          <MDBRow>
-            <MDBCol className="mr-md-auto text-center text-md-left mb-3 mb-md-0">
-              <Logo />
-            </MDBCol>
-            <MDBCol
-              size="12"
-              md="auto"
-              className="text-center text-md-left"
-              middle
-            >
-              Designed & Developed by <strong>Z Lutfi.</strong> Proudly powered
-              by Gatsby.js, Prismic.io and
-              <a href="https://www.netlify.com">
+    <MDBFooter color="primary" className="font-small pt-5">
+      <MDBContainer className="text-center pb-3">
+        <MDBRow className="justify-content-center py-3">
+          <MDBCol size="auto">
+            <strong>{data.prismicFooter.data.powered_by_title}</strong>
+          </MDBCol>
+          {data.prismicFooter.data.powered_by.map((brand, index) => (
+            <MDBCol key={index} size="auto">
+              <OutboundLink
+                href={brand.link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                alt={brand.image.alt}
+              >
                 <img
-                  src="https://www.netlify.com/img/global/badges/netlify-light.svg"
-                  alt="Deployed with Netlify"
+                  src={brand.image.url}
+                  alt={brand.image.alt}
+                  height="20px"
+                  className="mb-3"
                 />
-              </a>
+              </OutboundLink>
             </MDBCol>
-            <MDBCol
-              size="12"
-              md="auto"
-              className="text-center text-md-left"
-              middle
-            >
-              &copy; Copyright&nbsp;
-              {new Date().getFullYear()}
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </MDBFooter>
-    </>
+          ))}
+        </MDBRow>
+        <MDBRow>
+          <MDBCol>
+            <strong>{data.prismicFooter.data.copyright}</strong> - &copy;
+            Copyright&nbsp;
+            {new Date().getFullYear()}
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </MDBFooter>
   )
 }
 
 export default Footer
 
-// Inline Logo SVG
-const Logo = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 163.13 72.6"
-    className="footer-logo"
-  >
-    <defs>
-      <clipPath transform="translate(0 .02)">
-        <polygon points="163.13 72.58 91.3 72.58 91.3 -0.02 119.62 -0.02 119.62 64.44 163.13 64.1 163.13 72.58" />
-      </clipPath>
-    </defs>
-    <path
-      d="M86.17,72.58H0v-8.5L43.08,7.25,1.84,7.12,1.72,0H79.46V5.72L37.66,64.61l48.51-.51Z"
-      transform="translate(0 .02)"
-    />
-    <path
-      d="M163.13,72.58H91.3V0h28.32V64.44l43.51-.34Z"
-      transform="translate(0 .02)"
-    />
-    <polygon points="163.13 72.6 91.3 72.6 91.3 0 119.62 0 119.62 64.46 163.13 64.12 163.13 72.6" />
-    <g>
-      <path
-        d="M86.17,72.58H0v-8.5L43.08,7.25,1.84,7.12,1.72,0H79.46V5.72L37.66,64.61l48.51-.51Z"
-        transform="translate(0 .02)"
-      />
-    </g>
-  </svg>
-)
+Footer.propTypes = {
+  data: PropTypes.shape({
+    prismicFooter: PropTypes.shape({
+      data: PropTypes.shape({
+        powered_by: PropTypes.shape({
+          link: PropTypes.shape({
+            url: PropTypes.string.isRequired,
+          }),
+          image: PropTypes.shape({
+            alt: PropTypes.string.isRequired,
+            url: PropTypes.string.isRequired,
+          }),
+          copyright: PropTypes.string.isRequired,
+          powered_by_title: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+}
