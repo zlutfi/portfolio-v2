4907909d0f6d96c78808707f5b4a10fe998df17f
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-06 13:13:40 -0400

    Updated packages for PurgeCSS  support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,6 +103,17 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
+    // {
+    //   resolve: `gatsby-plugin-purgecss`,
+    //   options: {
+    //     printRejected: true, // Print removed selectors and processed file names
+    //     develop: true, // Enable while using `gatsby develop`
+    //     // tailwind: true, // Enable tailwindcss support
+    //     // whitelist: ["btn"], // Don't remove this selector
+    //     // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+    //     purgeOnly: ["components/", "/main.css", "styles/"], // Purge only these files/folders
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-sharp`,
