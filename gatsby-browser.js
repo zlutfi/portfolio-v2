f99a9934646b05e396aa7f1466c40fe333817bc2
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-17 15:51:44 -0400

    Added service worker message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,6 +11,17 @@ import "bootstrap-css-only/css/bootstrap.min.css"
 import "./src/styles/scss/mdb.scss"
 import "./src/styles/icons/fontawesome-free/css/all.min.css"
 
+// Displays a message when a service worker updates
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
+
 // Adds support for the Gatsby Image Background plugin
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
