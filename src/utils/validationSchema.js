9c065a89f673af6d213cfc00058060cdc3b43e71
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-24 12:07:41 -0400

    Adding comments

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -4,18 +4,22 @@ import * as Yup from "yup"
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
 const validationSchema = Yup.object().shape({
+  // Validate name
   name: Yup.string()
     .min(2, "This name is too short.")
     .max(50, "This name is too long.")
     .required("A name is required."),
+  // Validate email address
   email: Yup.string()
     .email("Please enter a valid email address.")
     .required("An email address is required."),
+  // Validate phone number
   phone: Yup.string()
     .matches(phoneRegExp, "Please enter a valid phone number.")
     .min(10, "This phone number is too short.")
     .max(50, "This phone number is too long.")
     .required("Please enter a phone number."),
+  // Validate message
   message: Yup.string().required("Please enter a message."),
 })
 
