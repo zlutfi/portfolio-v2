3281e629fbbd4c49f2dce64f90c13a3a3360146d
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-24 12:07:10 -0400

    Not needed

--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-import usWebDesignStandardsTheme from "typography-theme-us-web-design-standards"
-
-const typography = new Typography({
-  usWebDesignStandardsTheme,
-})
-
-export default typography
