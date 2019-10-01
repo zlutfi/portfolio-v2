d8727d5f0a4d1c1a9546b384d048c7a12f73c25f
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-01 16:00:11 -0400

    Simplifying code

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { MDBContainer, MDBRow, MDBCol, MDBJumbotron } from "mdbreact"
+import Img from "gatsby-image/withiepolyfill"
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBJumbotron,
+  MDBView,
+  MDBMask,
+} from "mdbreact"
 import Reveal from "react-reveal/Reveal"
-// import BackgroundImage from "gatsby-background-image"
+import BackgroundImage from "gatsby-background-image"
 
 const Header = ({
   title = this.props.title,
@@ -11,32 +19,54 @@ const Header = ({
   bgColor = "unique-color-dark",
 }) => (
   <>
-    {/* <BackgroundImage fluid={background} backgroundColor={`#eee`}> */}
-    <MDBJumbotron fluid id="page">
-      <MDBContainer>
-        <MDBRow className="py-5 my-5">
-          <MDBCol size="8" md="4" className="text-center mb-n5 mx-auto">
-            {/* <div
+    {/* <BackgroundImage fluid={background} backgroundColor={`#eee`}>
+      <MDBJumbotron fluid id="page">
+        <MDBContainer>
+          <MDBRow className="py-5 my-5">
+            <MDBCol size="8" md="4" className="text-center mb-n5 mx-auto">
+              <div
                 style={{ backgroundColor: "rgb(28, 78, 150)" }}
                 className="z-depth-1 rounded py-5"
-              > */}
-            <Reveal effect="text-focus-in">
-              <h1
-                className="h1-responsive font-weight-bold section-title"
-                style={{ color: "#222" }}
               >
-                {title}
-              </h1>
-            </Reveal>
-            {/* </div> */}
-            {/* <p className="lead">
+              <Reveal effect="text-focus-in">
+                <h1 className="h1-responsive font-weight-bold section-title">
+                  {title}
+                </h1>
+              </Reveal>
+              </div>
+              <p className="lead">
                   <strong>{subtitle}</strong>
-                </p> */}
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </MDBJumbotron>
-    {/* </BackgroundImage> */}
+                </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </MDBJumbotron>
+    </BackgroundImage> */}
+    <MDBView tag="header">
+      <Img
+        fluid={background}
+        objectFit="cover"
+        objectPosition="50% 50%"
+        className="kenburns-top"
+      />
+      <MDBMask className="flex-center" overlay="black-slight">
+        <MDBContainer className="py-3 py-md-5 mt-5 mt-md-4">
+          <MDBRow>
+            <MDBCol
+              size="12"
+              md="8"
+              className="text-center text-md-left mx-auto mx-md-0"
+            >
+              <Reveal effect="text-focus-in">
+                <h1 className="h1-responsive font-weight-bold section-title white-text">
+                  {title}
+                </h1>
+              </Reveal>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </MDBMask>
+    </MDBView>
   </>
 )
 
