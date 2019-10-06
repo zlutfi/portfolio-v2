b3b3ef3813de580c611d8d4e5c16eb76d76824bf
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-06 11:34:30 -0400

    Updated component file names

--- src/components/slices/index.js
+++ src/components/slices/index.js
@@ -2,21 +2,24 @@ import Text from "./text"
 import Code from "./code"
 import Image from "./img"
 import Quote from "./quote"
-import ImageRight from "./imgRight"
-import ImageLeft from "./imgLeft"
+import ImageRight from "./img-right"
+import ImageLeft from "./img-left"
 import Gallery from "./gallery"
-import TextCard from "./textCard"
+import TextCard from "./text-card"
 import Carousel from "./carousel"
-import ThreeColumns from "./threeColumns"
+import SwiperCarousel from "./swiper"
+import ThreeColumns from "./three-columns"
 import Columns from "./columns"
 import Divider from "./divider"
 
 // Homepage Slices
-import Capabilities from "./homepage/capabilities"
-import Hero from "./homepage/hero"
-import Posts from "./homepage/posts"
-import Projects from "./homepage/projects"
-import Contact from "./homepage/contact"
+import Capabilities from "./homepage/about-section"
+import Hero from "./homepage/hero-section"
+import Posts from "./homepage/posts-section"
+import Projects from "./homepage/projects-section"
+import Contact from "./homepage/contact-section"
+import Technology from "./homepage/tech-section"
+import Skills from "./homepage/skills-section"
 // Project Slices
 import Info from "../project/info"
 
@@ -30,6 +33,7 @@ export {
   ImageLeft,
   TextCard,
   Carousel,
+  SwiperCarousel,
   ThreeColumns,
   Columns,
   Divider,
@@ -39,6 +43,8 @@ export {
   Posts,
   Projects,
   Contact,
+  Skills,
+  Technology,
   // Project Slices
   Info,
 }
