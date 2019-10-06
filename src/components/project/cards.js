54f94d13d98c8d2bdcdd4fca78a62c33fca99fc0
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-06 11:36:33 -0400

    Layout changes

--- src/components/project/cards.js
+++ src/components/project/cards.js
@@ -2,13 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 import {
+  MDBContainer,
   MDBRow,
   MDBCol,
   MDBCard,
   MDBCardBody,
   MDBCardText,
   MDBCardTitle,
-  MDBCardFooter,
+  // MDBCardFooter,
   // MDBBadge,
   MDBBtn,
   MDBIcon,
@@ -19,133 +20,141 @@ import Img from "gatsby-image"
 import { Link } from "gatsby"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
-import Fade from "react-reveal/Fade"
+import Zoom from "react-reveal/Zoom"
+
+import posed, { PoseGroup } from "react-pose"
+
+const PersonItem = posed.div({
+  enter: {
+    opacity: 1,
+    scale: 1,
+    delay: ({ i }) => i * 400,
+    transition: {
+      y: { type: "spring", stiffness: 1000, damping: 15 },
+      default: { duration: 300 },
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0,
+    delay: ({ i }) => i * 300,
+  },
+})
 
 const Cards = ({ input }) => {
   return (
-    <MDBRow className="py-5">
-      {input.map((project, index) => {
-        return (
-          <MDBCol size="12" className="mb-4" key={index}>
-            <Fade bottom delay={200 * index}>
-              <MDBCard>
-                <MDBRow>
-                  <MDBCol md="6">
-                    {/* image section */}
-                    <Link
-                      to={project.node.url}
-                      title={project.node.data.title.text}
-                    >
-                      <MDBView hover zoom>
-                        <Img
-                          fluid={
-                            project.node.data.thumbnail.localFile
-                              .childImageSharp.fluid
-                          }
-                          alt={project.node.data.thumbnail.alt}
-                          className="rounded"
+    <MDBContainer>
+      <MDBRow className="py-5">
+        {/* <PoseGroup animateOnMount> */}
+        {input.map((project, i) => {
+          return (
+            <MDBCol size="12" md="6" className="mb-4" key={i}>
+              <Zoom delay={300 * i}>
+                <MDBCard>
+                  {/* image section */}
+                  <Link
+                    to={project.node.url}
+                    title={project.node.data.title.text}
+                  >
+                    <MDBView hover zoom>
+                      <Img
+                        fluid={
+                          project.node.data.thumbnail.localFile.childImageSharp
+                            .fluid
+                        }
+                        alt={project.node.data.thumbnail.alt}
+                        className="rounded"
+                      />
+                      <MDBMask className="flex-center" overlay="stylish-strong">
+                        <MDBIcon
+                          icon="search"
+                          size="4x"
+                          className="white-text"
                         />
-                        <MDBMask
-                          className="flex-center"
-                          overlay="stylish-strong"
-                        >
-                          <MDBIcon
-                            icon="search"
-                            size="4x"
-                            className="white-text"
-                          />
-                        </MDBMask>
-                      </MDBView>
-                    </Link>
-                  </MDBCol>
-                  <MDBCol md="6">
-                    <MDBCardBody>
-                      <MDBRow>
-                        <MDBCol size="12" className="order-md-last">
-                          {/* <div className="card-btn">
-                            <Link to={project.node.url}>
-                              <MDBBtn color="primary" tag="span">
-                                View Project{" "}
-                                <MDBIcon icon="folder-open" className="ml-2" />
-                              </MDBBtn>
-                            </Link>
-                          </div> */}
+                      </MDBMask>
+                    </MDBView>
+                  </Link>
+                  <MDBCardBody>
+                    <MDBRow>
+                      <MDBCol size="12" className="order-md-last">
+                        {/* <div className="card-btn">
+                                  <Link to={project.node.url}>
+                                    <MDBBtn color="primary" tag="span">
+                                      View Project{" "}
+                                      <MDBIcon icon="folder-open" className="ml-2" />
+                                    </MDBBtn>
+                                  </Link>
+                                </div> */}
 
-                          <MDBCardTitle className="font-weight-bold" tag="div">
-                            <MDBRow between>
-                              <MDBCol size="12">
-                                {project.node.data.title.text}
-                              </MDBCol>
-                              {/* <MDBCol size="auto">
-                                {project.node.tags.map((tag, index) => (
-                                  <MDBBadge
-                                    pill
-                                    color="dark"
-                                    className="ml-2"
-                                    key={index}
-                                  >
-                                    {tag}
-                                  </MDBBadge>
-                                ))}
-                              </MDBCol> */}
-                            </MDBRow>
-                          </MDBCardTitle>
-                          <MDBCardText>
-                            {project.node.data.subtitle}
-                          </MDBCardText>
-                          <MDBCardText>
-                            <MDBRow>
-                              <MDBCol size="auto">
-                                <OutboundLink
-                                  href="https://github.com"
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  title="Github"
+                        <MDBCardTitle className="font-weight-bold" tag="div">
+                          <MDBRow between>
+                            <MDBCol size="12">
+                              {project.node.data.title.text}
+                            </MDBCol>
+                            {/* <MDBCol size="auto">
+                                      {project.node.tags.map((tag, index) => (
+                                        <MDBBadge
+                                          pill
+                                          color="dark"
+                                          className="ml-2"
+                                          key={index}
+                                        >
+                                          {tag}
+                                        </MDBBadge>
+                                      ))}
+                                    </MDBCol> */}
+                          </MDBRow>
+                        </MDBCardTitle>
+                        <MDBCardText>{project.node.data.subtitle}</MDBCardText>
+                        <MDBCardText tag="div">
+                          <MDBRow>
+                            <MDBCol size="auto">
+                              <OutboundLink
+                                href="https://github.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                title="Github"
+                              >
+                                <MDBBtn
+                                  color="elegant"
+                                  tag="span"
+                                  size="sm"
+                                  className="btn-rounded"
                                 >
-                                  <MDBBtn
-                                    color="elegant"
-                                    tag="span"
-                                    size="sm"
-                                    className="btn-rounded"
-                                  >
-                                    <MDBIcon
-                                      fab
-                                      icon="github"
-                                      className="mr-2"
-                                    />{" "}
-                                    View Code
-                                  </MDBBtn>
-                                </OutboundLink>
-                              </MDBCol>
-                              <MDBCol size="auto">
-                                <Link to={project.node.url}>
-                                  <MDBBtn
-                                    color="primary"
-                                    tag="span"
-                                    size="sm"
-                                    className="btn-rounded"
-                                  >
-                                    View Project{" "}
-                                    <MDBIcon
-                                      icon="folder-open"
-                                      className="ml-2"
-                                    />
-                                  </MDBBtn>
-                                </Link>
-                              </MDBCol>
-                            </MDBRow>
-                          </MDBCardText>
-                        </MDBCol>
-                      </MDBRow>
-                    </MDBCardBody>
-                  </MDBCol>
-                </MDBRow>
-              </MDBCard>
-            </Fade>
-          </MDBCol>
-        )
-      })}
-    </MDBRow>
+                                  <MDBIcon fab icon="github" className="mr-2" />{" "}
+                                  View Code
+                                </MDBBtn>
+                              </OutboundLink>
+                            </MDBCol>
+                            <MDBCol size="auto">
+                              <Link to={project.node.url}>
+                                <MDBBtn
+                                  color="primary"
+                                  tag="span"
+                                  size="sm"
+                                  className="btn-rounded"
+                                >
+                                  View Project{" "}
+                                  <MDBIcon
+                                    icon="folder-open"
+                                    className="ml-2"
+                                  />
+                                </MDBBtn>
+                              </Link>
+                            </MDBCol>
+                          </MDBRow>
+                        </MDBCardText>
+                      </MDBCol>
+                    </MDBRow>
+                  </MDBCardBody>
+                </MDBCard>
+              </Zoom>
+            </MDBCol>
+          )
+        })}
+        {/* </PoseGroup> */}
+      </MDBRow>
+    </MDBContainer>
   )
 }
 
@@ -162,7 +171,7 @@ export default Cards
 // className="mb-4"
 // key={document.node.id}
 // >
-// <Fade bottom delay={200 * index}>
+// <Zoom bottom delay={200 * index}>
 //   <MDBCard className="card-zoom">
 //     <MDBRow>
 //       <MDBCol md="6">
@@ -225,5 +234,5 @@ export default Cards
 //       </MDBCol>
 //     </MDBRow>
 //   </MDBCard>
-// </Fade>
+// </Zoom>
 // </MDBCol>
