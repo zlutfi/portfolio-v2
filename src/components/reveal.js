3215464aa8d8fbdf8974a93272d35d6c7655fdb8
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-14 09:58:23 -0400

    Removing unused components and schema

--- src/components/reveal.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Zoom from "react-reveal/Zoom"
-
-const Reveal = ({children}) = (
-        <Zoom delay={400}>
-           {children} 
-        </Zoom>
-    )
-export default Reveal
