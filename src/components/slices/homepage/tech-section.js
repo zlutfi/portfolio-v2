98a67f048abc45c009ec3192235d4f4b6d0b00e1
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-15 17:37:13 -0400

    Updated layout

--- src/components/slices/homepage/tech-section.js
+++ src/components/slices/homepage/tech-section.js
@@ -1,36 +1,67 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import SectionTitle from "../../section-title"
-import Logo from "../../../images/code.svg"
+// import Logo from "../../../images/code.svg"
+// import Img from "gatsby-image"
 
 const Tech = ({ input }) => (
-  <section style={{ backgroundColor: "#f7f8fa" }}>
+  <section style={{ backgroundColor: "#eee" }}>
     <MDBContainer
+      fluid
       className="py-4 py-md-5 mx-auto text-center"
-      style={{ backgroundColor: "#f7f8fa" }}
+      style={{ backgroundColor: "#eee" }}
     >
-      {/* Title section */}
-      <SectionTitle
-        title={input.primary.technology_section_title.text}
-        subtitle={input.primary.technology_section_subtitle.html}
-      />
-      <img src={Logo} alt="Logo" />
-      {/* List all technologies dynamically */}
-      <MDBRow>
-        {input.items.map((tech, index) => (
-          <MDBCol size="3" md="2" key={index}>
-            {/* Tech icon */}
-            <FontAwesomeIcon
-              icon={["fab", tech.technology_icon]}
-              size="3x"
-              className="text-primary my-4"
-            />
-            {/* Tech header */}
-            <h5 className="grey-text">{tech.technology_title.text}</h5>
-          </MDBCol>
-        ))}
+      <MDBRow between>
+        {/* Tech image section */}
+        <MDBCol size="12" md="5">
+          <SectionTitle
+            title={input.primary.technology_section_title.text}
+            subtitle={input.primary.technology_section_subtitle.html}
+          />
+          <MDBBtn color="primary" rounded>
+            Learn More
+          </MDBBtn>
+          {/* <Img
+            fluid={
+              input.primary.technology_section_image.localFile.childImageSharp
+                .fluid
+            }
+            alt={input.primary.technology_section_image.alt}
+            className="ml-n5"
+          /> */}
+          {/* <img src={Logo} alt="Logo" className="img-fluid ml-n5" /> */}
+        </MDBCol>
+        <MDBCol size="12" md="7">
+          {/* List all technologies dynamically */}
+          <MDBRow>
+            {/* Title section */}
+            <MDBCol size="12">
+              {/* <SectionTitle
+                title={input.primary.technology_section_title.text}
+                subtitle={input.primary.technology_section_subtitle.html}
+              /> */}
+              {/* <h2>{input.primary.technology_section_title.text}</h2>
+              <p className="lead grey-text">
+                {input.primary.technology_section_subtitle.text}
+              </p> */}
+            </MDBCol>
+            {/* Tech Icons section */}
+            {input.items.map((tech, index) => (
+              <MDBCol size="4" key={index}>
+                {/* Tech icon */}
+                <FontAwesomeIcon
+                  icon={["fab", tech.technology_icon]}
+                  size="3x"
+                  className="text-primary my-4"
+                />
+                {/* Tech header */}
+                <h5 className="grey-text">{tech.technology_title.text}</h5>
+              </MDBCol>
+            ))}
+          </MDBRow>
+        </MDBCol>
       </MDBRow>
     </MDBContainer>
   </section>
