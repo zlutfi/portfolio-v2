67b7effc398c36a50e178964d268341cfd76a751
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-15 15:21:57 -0400

    Resolved tree shaking issue

---
+++ src/styles/scss/icons.js
@@ -0,0 +1,63 @@
+// Font awesome Library
+import { library } from "@fortawesome/fontawesome-svg-core"
+// Brands
+// import {
+//   faHtml5,
+//   faCss3Alt,
+//   faJs,
+//   faAdobe,
+//   faReact,
+//   faNodeJs,
+//   faGithub,
+// } from "@fortawesome/free-brands-svg-icons"
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons"
+import { faCss3Alt } from "@fortawesome/free-brands-svg-icons"
+import { faJs } from "@fortawesome/free-brands-svg-icons"
+import { faAdobe } from "@fortawesome/free-brands-svg-icons"
+import { faReact } from "@fortawesome/free-brands-svg-icons"
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+// Solid
+// import {
+//   faCode,
+//   faMobileAlt,
+//   faTachometerAlt,
+//   faUniversalAccess,
+//   faEnvelope,
+//   faFolderOpen,
+//   faPaperPlane,
+//   faDesktop,
+//   faArrowAltCircleLeft,
+//   faArrowAltCircleRight,
+// } from "@fortawesome/free-solid-svg-icons"
+import { faCode } from "@fortawesome/free-solid-svg-icons"
+import { faMobileAlt } from "@fortawesome/free-solid-svg-icons"
+import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons"
+import { faUniversalAccess } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faFolderOpen } from "@fortawesome/free-solid-svg-icons"
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons"
+import { faDesktop } from "@fortawesome/free-solid-svg-icons"
+import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons"
+import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons"
+
+// Library
+library.add(
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faAdobe,
+  faReact,
+  faNodeJs,
+  faCode,
+  faMobileAlt,
+  faTachometerAlt,
+  faUniversalAccess,
+  faEnvelope,
+  faFolderOpen,
+  faPaperPlane,
+  faGithub,
+  faDesktop,
+  faArrowAltCircleLeft,
+  faArrowAltCircleRight
+)
