1654493b229a53977be5961779eaffc8877b6f1a
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-15 13:09:45 -0400

    Added webpack bundle analyser plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -209,6 +209,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
+      options: {
+        devMode: true,
+      },
+    },
     `gatsby-plugin-netlify`,
     "gatsby-plugin-netlify-cache",
   ],
