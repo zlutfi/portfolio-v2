2d83f682aa74361c0f90ebb01af3c7e267fd8a6f
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-21 09:02:47 -0400

    Updated shape divider props

--- src/components/shape-divider.js
+++ src/components/shape-divider.js
@@ -3,19 +3,25 @@ import React from "react"
 const ShapeDivider = props => {
   // Define props
   const fill = props.fill
+  const fill2 = props.fill2
   // If rotate prop is true return this
   if (props.rotateY === true)
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
-        style={{ transform: "rotateY(180deg)" }}
+        style={{
+          transform: "rotateY(180deg)",
+          // backgroundColor: `${bgcolor}`,
+          margin: "-2px",
+        }}
       >
+        <rect width="1440" height="320" fill={fill2} />
         <path
           // Replace default fill color with fill prop string value
           fill={fill}
           fillOpacity="1"
-          d="M0,32L80,69.3C160,107,320,181,480,202.7C640,224,800,192,960,165.3C1120,139,1280,117,1360,106.7L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          d="M0,32,80,69.3C160,107,320,181,480,202.7,640,224,800,192,960,165.3c160-26.3,320-48.3,400-58.6L1440,96V320H0Z"
         ></path>
       </svg>
     )
@@ -24,25 +30,35 @@ const ShapeDivider = props => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
-        style={{ transform: "rotateX(180deg)" }}
+        style={{
+          transform: "rotateX(180deg)",
+          // backgroundColor: `${bgcolor}`,
+          margin: "-2px",
+        }}
       >
+        <rect width="1440" height="320" fill={fill2} />
         <path
           // Replace default fill color with fill prop string value
           fill={fill}
           fillOpacity="1"
-          d="M0,32L80,69.3C160,107,320,181,480,202.7C640,224,800,192,960,165.3C1120,139,1280,117,1360,106.7L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          d="M0,32,80,69.3C160,107,320,181,480,202.7,640,224,800,192,960,165.3c160-26.3,320-48.3,400-58.6L1440,96V320H0Z"
         ></path>
       </svg>
     )
   // If rotate prop is false return this
   else
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        style={{ margin: "-2px" }}
+      >
+        <rect width="1440" height="320" fill={fill2} />
         <path
           // Replace default fill color with fill prop string value
           fill={fill}
           fillOpacity="1"
-          d="M0,32L80,69.3C160,107,320,181,480,202.7C640,224,800,192,960,165.3C1120,139,1280,117,1360,106.7L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          d="M0,32,80,69.3C160,107,320,181,480,202.7,640,224,800,192,960,165.3c160-26.3,320-48.3,400-58.6L1440,96V320H0Z"
         ></path>
       </svg>
     )
